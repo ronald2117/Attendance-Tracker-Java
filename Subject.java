7c46aab16feef5a0da61a318ea7657380818953e
ronald2117 <abel.ronald2117@gmail.com>
2024-04-29 18:28:55 +0800

    Refactor Subject class to encapsulate fields and add getter and setter methods

--- Subject.java
+++ Subject.java
@@ -2,12 +2,61 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Subject {
-    String name;
-    ClassTime time;
-    String teacher;
-    String room;
-    String section;
-    String subjectCode;
-    ArrayList<Student> students;
-    HashMap<Date, ArrayList<Attendance>> attendance;
+    private String name;
+    private ClassTime time;
+    private String teacher;
+    private String room;
+    private String section;
+    private String subjectCode;
+    private ArrayList<Student> students;
+    private HashMap<Date, ArrayList<Attendance>> attendance;
+
+    public Subject(String name, ClassTime time, String teacher, String room, String section, String subjectCode) {
+        this.name = name;
+        this.time = time;
+        this.teacher = teacher;
+        this.room = room;
+        this.section = section;
+        this.subjectCode = subjectCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ClassTime getTime() {
+        return time;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public String getSubjectCode() {
+        return subjectCode;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public HashMap<Date, ArrayList<Attendance>> getAttendance() {
+        return attendance;
+    }
+
+    public void addAttendance(Date date, ArrayList<Attendance> attendanceList) {
+        attendance.put(date, attendanceList);
+    }
 }
